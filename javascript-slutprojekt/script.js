d296cb24183b0f5dc78f57ff73ea7045d1b387fd
hampusq <hampus.aberg@elev.ga.ntig.se>
2020-05-27 10:44:45 +0200

    2020-05-27

--- javascript-slutprojekt/script.js
+++ javascript-slutprojekt/script.js
@@ -4,6 +4,7 @@ const eInput = document.querySelector("#input");
 const eContent1 = document.querySelector(".content1");
 const eContent2 = document.querySelector(".content2");
 const eContent3 = document.querySelector(".content3");
+const eError = document.querySelector("#error");
 
 eContent1.classList.add("hidden");
 eContent2.classList.add("hidden");
@@ -12,18 +13,27 @@ eContent3.classList.add("hidden");
 
 var intresseVal = eInput.value;
 
-eButton.addEventListener("click", function intresse() {
 
-    if (eInput.value == "golf") {
+
+eButton.addEventListener("click", function() {
+    // Array med små bokstaver
+    let intressenSmall = ['golf', 'fotboll', 'mma']
+
+    // Array första bokstaven stor
+    let intressenBig = ['Golf', 'Fotboll', 'Mma']
+
+    // Skriv ut det intresse användaren skriver in
+    if (eInput.value == intressenSmall[0] || eInput.value == intressenBig[0]) {
         eContent1.classList.add("show");
-    }
-    if (eInput.value == "fotboll") {
-        eContent2.classList.add("show");
-    }
-    if (eInput.value == "mma") {
-        eContent3.classList.add("show");
-    }
-    else {
-        eInput.value = "Fel"
+    } else {
+        if (eInput.value == intressenSmall[1] || eInput.value == intressenBig[1]) {
+            eContent2.classList.add("show");
+        } else {
+            if (intressenSmall[2] || eInput.value == intressenBig[2]) {
+                eContent3.classList.add("show");
+            } else {
+                eError.innerHTML = "Fel, testa att skriv: golf, fotboll eller mma"
+            }
+        }
     }
 });
